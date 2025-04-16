33fbbd350c02da17e94cf507f0519386eb0ddabd
Alex Barcelo <alex@betarho.net>
2025-04-16 15:36:08 +0200

    Preparing 1.1.0 release

--- fastapi_keycloak/__init__.py
+++ fastapi_keycloak/__init__.py
@@ -1,6 +1,6 @@
 """Keycloak API Client for integrating authentication and authorization with FastAPI"""
 
-__version__ = "1.1.0-rc"
+__version__ = "1.1.0"
 
 from fastapi_keycloak.api import FastAPIKeycloak
 from fastapi_keycloak.model import (
